291b0b62a90a9fce2adb9c91265a3d5ca9972713
Petar Petrov <ppetrov12@abv.bg>
2022-10-12 19:26:29 +0300

    Refactor JS and add custom  html attributes "data-key"

--- script.js
+++ script.js
@@ -1,98 +1,29 @@
-
-const boom = document.querySelector("#boom");
-const clap = document.querySelector("#clap");
-const hihat = document.querySelector("#hihat");
-const kick = document.querySelector("#kick");
-const openhat = document.querySelector("#openhat");
-const ride = document.querySelector("#ride");
-const snare = document.querySelector("#snare");
-const tink = document.querySelector("#tink");
-const tom = document.querySelector("#tom");
+const keys = document.querySelectorAll('.keys');
 
 
-document.addEventListener('keydown', playSound);
-document.addEventListener('keyup', removeEffect);
-
-boom.addEventListener('click', () => new Audio('sounds/boom.wav').play());
-clap.addEventListener('click', () => new Audio('sounds/clap.wav').play());
-hihat.addEventListener('click', () => new Audio('sounds/hihat.wav').play());
-kick.addEventListener('click', () => new Audio('sounds/kick.wav').play());
-openhat.addEventListener('click', () => new Audio('sounds/openhat.wav').play());
-ride.addEventListener('click', () => new Audio('sounds/ride.wav').play());
-snare.addEventListener('click', () => new Audio('sounds/snare.wav').play());
-tink.addEventListener('click', () => new Audio('sounds/tink.wav').play());
-tom.addEventListener('click', () => new Audio('sounds/tom.wav').play());
+window.addEventListener("keydown", playKeystroke);
+keys.forEach(key => key.addEventListener('click', ()=> playClick(key)));
+keys.forEach(key => key.ontransitionend = () => key.classList.remove('active'));
 
 
-    function playSound(event) {
-        switch (event.key.toLowerCase()) {
-            case 'a':
-                clap.classList.toggle('active');
-                new Audio('sounds/clap.wav').play();
-                break;
-            case 's':
-                hihat.classList.toggle('active');
-                new Audio('sounds/hihat.wav').play();
-                break;
-            case 'd':
-                kick.classList.toggle('active');
-                new Audio('sounds/kick.wav').play();
-                break;
-            case 'f':
-                openhat.classList.toggle('active');
-                new Audio('sounds/openhat.wav').play();
-                break;
-            case 'g':
-                boom.classList.toggle('active');
-                new Audio('sounds/boom.wav').play();
-                break;
-            case 'h':
-                ride.classList.toggle('active');
-                new Audio('sounds/ride.wav').play();
-                break;
-            case 'j':
-                snare.classList.toggle('active');
-                new Audio('sounds/snare.wav').play();
-                break;
-            case 'k':
-                tom.classList.toggle('active');
-                new Audio('sounds/tom.wav').play();
-                break;
-            case 'l':
-                tink.classList.toggle('active');
-                new Audio('sounds/tink.wav').play();
-                break;
-        }
-    }
-
-    function removeEffect(event){
-    switch (event.key.toLowerCase()) {
-        case 'a':
-            clap.classList.toggle('active');
-            break;
-        case 's':
-            hihat.classList.toggle('active');
-            break;
-        case 'd':
-            kick.classList.toggle('active');
-            break;
-        case 'f':
-            openhat.classList.toggle('active');
-            break;
-        case 'g':
-            boom.classList.toggle('active');
-            break;
-        case 'h':
-            ride.classList.toggle('active');
-            break;
-        case 'j':
-            snare.classList.toggle('active');
-            break;
-        case 'k':
-            tom.classList.toggle('active');
-            break;
-        case 'l':
-            tink.classList.toggle('active');
-            break;
-    }
+function playClick(key) {
+    const att  = key.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${att}"]`);
+    key.classList.add('active');
+    audio.currentTime = 0;
+    audio.play();
 }
+
+
+function playKeystroke(e)  {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if(audio==null)
+        return;
+    key.classList.add('active');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+
